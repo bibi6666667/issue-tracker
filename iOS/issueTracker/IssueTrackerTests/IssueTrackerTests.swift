fdc4b816f8a7dfbdf1fafba265dabd8abbe5c23a
Ji-Sun Kim <jisunkim98@gmail.com>
2021-06-10 16:53:04 +0900

    feat: project rename

--- iOS/issueTracker/IssueTrackerTests/IssueTrackerTests.swift
+++ iOS/issueTracker/IssueTrackerTests/IssueTrackerTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import issueTracker
+@testable import IssueTracker
 
-class issueTrackerTests: XCTestCase {
+class IssueTrackerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
