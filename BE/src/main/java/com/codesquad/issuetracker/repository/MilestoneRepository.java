0db04a9c9ffc06961d1b9ba7d6c1285653ca07b9
bibi <bibi6666667@kakao.com>
2021-06-22 12:11:52 +0900

    chore : Domain 중심으로 프로젝트 패키지 구조 개편

--- BE/src/main/java/com/codesquad/issuetracker/repository/MilestoneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codesquad.issuetracker.repository;
-
-import com.codesquad.issuetracker.domain.Milestone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
-}
